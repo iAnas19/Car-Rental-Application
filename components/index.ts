70440c103a56feb3923dc1b89648c352d5576f4e
Muhammad Anas <anasarif216@gmail.com>
2023-11-11 18:31:35 +0500

    add search manufacturer

--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,16 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-export { Navbar, Footer, Hero, CustomButton };
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+
+export {
+  Navbar,
+  Footer,
+  Hero,
+  CustomButton,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtonProps {
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
 }
+
+export type SearchManufacturerProps = {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+};
